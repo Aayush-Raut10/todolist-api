2da5b73867b1e69afec2c7d6f633aaa58d9ab60f
Aayush-Raut10 <aayushraut209@gmail.com>
2026-04-08 17:45:33 +0545

    Configuring environment variables in .env file

--- app/database.py
+++ app/database.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # SQLite URL
-SQLALCHEMY_DATABASE_URL = "sqlite:///./mydbtest.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Create engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
