d34cac4d30711b5e2fb9a77d64f765240984c2b1
Aayush-Raut10 <aayushraut209@gmail.com>
2026-07-13 22:49:29 +0545

    fix: mail configuration

--- app/core/mail.py
+++ app/core/mail.py
@@ -1,78 +1,31 @@
 import os
+import resend
 from dotenv import load_dotenv
-from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
 
 load_dotenv()
 
-
-configuration = ConnectionConfig(
-
-    MAIL_USERNAME = os.getenv("MAIL_USERNAME"),
-    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD"),
-    MAIL_FROM = os.getenv("MAIL_FROM"),
-    
-    MAIL_SERVER = "smtp.gmail.com",
-    MAIL_PORT = 465,
-
-    MAIL_STARTTLS = True,
-    MAIL_SSL_TLS = False,
-
-     USE_CREDENTIALS=True,
-    VALIDATE_CERTS=True,
-
-)
+resend.api_key = os.getenv("RESEND_API_KEY")
 
 
-async def send_email(recipient:str, subject:str, body:str):
+async def send_email(recipient: str, subject: str, body: str):
     """
-    This function runs in the background after the user gets a response
-
+    Sends an email using the Resend API.
     """
-    
 
-    # prepare the email
-    message = MessageSchema(
-
-            recipients = [recipient],
-            subject = subject,
-            body = body,
-            subtype = "html"
-
-        )
-    
-    fm = FastMail(config=configuration)
-   
-
-    # Try sending the mail
     try:
-        print(f"sending email to {recipient}...")
-        import socket
+        print(f"Sending email to {recipient}...")
 
-        try:
-            ip = socket.gethostbyname("smtp.gmail.com")
-            print("Resolved:", ip)
-        except Exception as e:
-            print("DNS failed:", e)
+        response = resend.Emails.send(
+            {
+                "from": "dolindrabahadurraut@gmail",
+                "to": recipient,
+                "subject": subject,
+                "html": body,
+            }
+        )
 
-            import socket
-
-        import socket
-
-        for host, port in [
-            ("google.com", 443),
-            ("api.github.com", 443),
-            ("smtp.gmail.com", 587),
-        ]:
-            try:
-                socket.create_connection((host, port), timeout=10)
-                print(f"{host}:{port} -> OK")
-            except Exception as e:
-                print(f"{host}:{port} -> {repr(e)}")
-        await fm.send_message(message=message)
         print("Email sent successfully!")
+        print(response)
 
     except Exception as e:
-        print(f"Mail failed: {e}")
-
-   
-        
+        print(f"Email failed: {e}")
