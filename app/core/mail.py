48ec2337778514385d4aae5c99c1454fd3a3b158
Aayush-Raut10 <aayushraut209@gmail.com>
2026-07-13 22:09:08 +0545

    fix: mail configuration

--- app/core/mail.py
+++ app/core/mail.py
@@ -10,10 +10,15 @@ configuration = ConnectionConfig(
     MAIL_USERNAME = os.getenv("MAIL_USERNAME"),
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD"),
     MAIL_FROM = os.getenv("MAIL_FROM"),
-    MAIL_PORT = 587,
+    
     MAIL_SERVER = "smtp.gmail.com",
+    MAIL_PORT = 587,
+
     MAIL_STARTTLS = True,
-    MAIL_SSL_TLS = False
+    MAIL_SSL_TLS = False,
+    
+     USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True,
 
 )
 
